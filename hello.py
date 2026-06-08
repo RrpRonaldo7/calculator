e3c6e3b2897507bf8d943bc92191ed94fc43090b
rrpronaldo <rohitrpandey0@gmail.com>
2026-06-08 23:17:29 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+a=int(input("enter a no.:"))
+b=int(input("enter another no.:"))
+c=input("enter your operation:")
+if c== "+":
+    print(a+b)
+if c== "-":
+    print(a-b)
+if c== "*":
+    print(a*b)
+if c== "/":
+    print(a/b)
+else:
+    print("operator not defined")
